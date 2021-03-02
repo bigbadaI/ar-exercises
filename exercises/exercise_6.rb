17309c49db15255dc42cd6640253c77f5b981941
BigBadAI <flashmefusion@hotmail.ca>
2021-03-02 00:18:42 +0000

    Practise with Ruby active record on databases

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,18 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+Store.has_many :employees
+Employee.belongs_to :store
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Frank", last_name: "Gal", hourly_rate: 45)
+@store2.employees.create(first_name: "Bruce", last_name: "Wayne", hourly_rate: 100)
+@store2.employees.create(first_name: "Alfred", last_name: "Pennyworth", hourly_rate: 60)
+@store4.employees.create(first_name: "Tony", last_name: "Stark", hourly_rate: 150)
+@store4.employees.create(first_name: "Pepper", last_name: "Pots", hourly_rate: 60)
+@store5.employees.create(first_name: "Arthur", last_name: "Virani", hourly_rate: 50)
+@store5.employees.create(first_name: "Benny", last_name: "Virani", hourly_rate: 60)
+@store5.employees.create(first_name: "Jules", last_name: "Virani", hourly_rate: 65)
+@store6.employees.create(first_name: "Kirk", last_name: "Coban", hourly_rate: 60)
+@store6.employees.create(first_name: "Garth", last_name: "Brooks", hourly_rate: 60)
+@store6.employees.create(first_name: "Brooks", last_name: "Dun", hourly_rate: 75)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,25 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates_numericality_of :hourly_rate, :greater_than =>  40
+  validates_numericality_of :hourly_rate, :less_than =>  200
+  validates :belongs_to,
+  presence: true
+end
+
+class Store < ActiveRecord::Base
+  validates :name, presence: true, length: { minimum: 3 }
+  validates_numericality_of :annual_revenue, :greater_than => 0
+end
+
+p "Please enter a store name"
+ans = $stdin.gets.chomp
+
+@storeans = Store.create(name: "#{ans}")
+
+@storeans.errors.full_messages.each do |message| 
+  p message 
+end
